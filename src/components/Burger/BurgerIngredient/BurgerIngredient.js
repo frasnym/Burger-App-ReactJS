0321de7c3eef85fa0182733c20e7fb68050f6945
frasnym <frastyawan.nym@gmail.com>
2020-12-11 12:34:58 +0800

    Prop Type Validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,42 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 import "./BurgerIngredient.css";
 
-export default function BurgerIngredient(props) {
-	let ingredient = null;
+class BurgerIngredient extends Component {
+	render() {
+		let ingredient = null;
 
-	switch (props.type) {
-		case "bread-bottom":
-			ingredient = <div className="BreadBottom"></div>;
-			break;
+		switch (this.props.type) {
+			case "bread-bottom":
+				ingredient = <div className="BreadBottom"></div>;
+				break;
 
-		case "bread-top":
-			ingredient = (
-				<div className="BreadTop">
-					<div className="Seeds1"></div>
-					<div className="Seeds2"></div>
-				</div>
-			);
-			break;
-		case "meat":
-			ingredient = <div className="Meat"></div>;
-			break;
+			case "bread-top":
+				ingredient = (
+					<div className="BreadTop">
+						<div className="Seeds1"></div>
+						<div className="Seeds2"></div>
+					</div>
+				);
+				break;
+			case "meat":
+				ingredient = <div className="Meat"></div>;
+				break;
 
-		case "cheese":
-			ingredient = <div className="Cheese"></div>;
-			break;
+			case "cheese":
+				ingredient = <div className="Cheese"></div>;
+				break;
 
-		case "bacon":
-			ingredient = <div className="Bacon"></div>;
-			break;
+			case "bacon":
+				ingredient = <div className="Bacon"></div>;
+				break;
 
-		case "salad":
-			ingredient = <div className="Salad"></div>;
-			break;
+			case "salad":
+				ingredient = <div className="Salad"></div>;
+				break;
 
-		default:
-			ingredient = null;
-			break;
+			default:
+				ingredient = null;
+				break;
+		}
+		return ingredient;
 	}
-	return ingredient;
 }
+
+BurgerIngredient.propTypes = {
+	type: propTypes.string.isRequired,
+};
+
+export default BurgerIngredient;
