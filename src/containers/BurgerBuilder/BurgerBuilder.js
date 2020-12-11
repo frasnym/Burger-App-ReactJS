bef0f6f33fadcb876b9924918f38d18b56d88023
frasnym <frastyawan.nym@gmail.com>
2020-12-11 12:43:31 +0800

    Setup Burger Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import Burger from "../../components/Burger/Burger";
+
 export default class BurgerBuilder extends Component {
 	render() {
 		return (
 			<>
-				<div>Burger</div>
+				<Burger />
 				<div>Build Control</div>
 			</>
 		);
