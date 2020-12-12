a991ca6708bdfe7bd08d5af2fab0139c320fd56d
frasnym <frastyawan.nym@gmail.com>
2020-12-12 13:03:59 +0800

    Javascript Array Reduce

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import "./Burger.css";
 
 export default function Burger(props) {
-	const transformedIngredients = Object.keys(props.ingredient).map(
-		(igKey) => {
+	let transformedIngredients = Object.keys(props.ingredient)
+		.map((igKey) => {
 			return [...Array(props.ingredient[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />;
 			}); // [,]
-		}
-	);
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (transformedIngredients.length <= 0) {
+		transformedIngredients = <p>Please start add ingredient</p>;
+	}
 
 	return (
 		<div className="Burger">
