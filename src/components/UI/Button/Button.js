19f3ad091c49331cbac61a302a80b7f8c85f9c9e
frasnym <frastyawan.nym@gmail.com>
2020-12-14 09:28:37 +0800

    Button Component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Button.css";
+
+export default function Button(props) {
+	return (
+		<button
+			className={["Button", props.btnType].join(" ")}
+			onClick={props.clicked}
+		>
+			{props.children}
+		</button>
+	);
+}
