1d31c1fe15ed7eebd94b0aa37c8326c9fb02f006
frasnym <frastyawan.nym@gmail.com>
2020-12-14 11:23:19 +0800

    SideDrawer Toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,20 +7,26 @@ import "./Layout.css";
 
 export default class Layout extends Component {
 	state = {
-		showSideDrawer: true,
+		showSideDrawer: false,
 	};
 
-	SideDrawerClosedHandler = () => {
+	sideDrawerClosedHandler = () => {
 		this.setState({ showSideDrawer: false });
+	};
+
+	sideDrawerToggleHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !this.state.showSideDrawer };
+		});
 	};
 
 	render() {
 		return (
 			// Fragment wraping components
 			<>
-				<Toolbar />
+				<Toolbar drawerToggleClick={this.sideDrawerToggleHandler} />
 				<SideDrawer
-					closed={this.SideDrawerClosedHandler}
+					closed={this.sideDrawerClosedHandler}
 					open={this.state.showSideDrawer}
 				/>
 				<main className="content">{this.props.children}</main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 import "./Toolbar.css";
 
 export default function Toolbar(props) {
 	return (
 		<header className="Toolbar">
-			<div>MENU</div>
+			<DrawerToggle clicked={props.drawerToggleClick} />
 			<div className="LogoToolbar">
 				<Logo />
 			</div>
