6fcc1ecf14e9cc7ab90febeaaac93caf997fb048
frasnym <frastyawan.nym@gmail.com>
2020-12-14 10:45:00 +0800

    Responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
+
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 import "./Layout.css";
 
 export default function Layout(props) {
 	return (
-		// Auxiliary wraping components
+		// Fragment wraping components
 		<>
 			<Toolbar />
+			<SideDrawer />
 			<main className="content">{props.children}</main>
 		</>
 	);
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+import "./SideDrawer.css";
+
+export default function SideDrawer() {
+	return (
+		<div className="SideDrawer">
+			<Logo />
+			<nav>
+				<NavigationItems />
+			</nav>
+		</div>
+	);
+}
