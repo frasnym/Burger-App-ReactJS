ecd1c57d8000fa3bd316951cd3d5fa6d67db035d
frasnym <frastyawan.nym@gmail.com>
2020-12-17 10:38:31 +0800

    Globally Handling Axios Error

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../utils/axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../components/HOC/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -14,7 +15,7 @@ const INGREDIENT_PRICES = {
 	meat: 1.3,
 };
 
-export default class BurgerBuilder extends Component {
+class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
 			salad: 0,
@@ -167,3 +168,5 @@ export default class BurgerBuilder extends Component {
 		);
 	}
 }
+
+export default withErrorHandler(BurgerBuilder, axios);
