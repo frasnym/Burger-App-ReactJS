272ed2c9ed6578861d3a53f2b8ee23bb4483134f
frasnym <frastyawan.nym@gmail.com>
2020-12-22 14:47:33 +0800

    Setup Orders Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 export default class App extends Component {
 	state = {
@@ -21,6 +22,7 @@ export default class App extends Component {
 			<Layout>
 				<Switch>
 					<Route path="/checkout" component={Checkout} />
+					<Route path="/orders" component={Orders} />
 					<Route path="/" exact component={BurgerBuilder} />
 				</Switch>
 			</Layout>
---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Order.css";
+
+export default function Order() {
+	return (
+		<div className="Order">
+			<p>Ingredients: Salad (1)</p>
+			<p>
+				Price: <strong>USD 99.33</strong>
+			</p>
+		</div>
+	);
+}
