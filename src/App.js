b62aa87145ec48c2154cefb28c985e0bf3a999e8
frasnym <frastyawan.nym@gmail.com>
2020-12-28 14:19:21 +0800

    Auth Form Setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
+import Auth from "./containers/Auth/Auth";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
@@ -21,6 +22,7 @@ export default class App extends Component {
 		return (
 			<Layout>
 				<Switch>
+					<Route path="/auth" component={Auth} />
 					<Route path="/checkout" component={Checkout} />
 					<Route path="/orders" component={Orders} />
 					<Route path="/" exact component={BurgerBuilder} />
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ export default function NavigationItems() {
 		<ul className="NavigationItems">
 			<NavigationItem link="/">Burget Builder</NavigationItem>
 			<NavigationItem link="/orders">Orders</NavigationItem>
+			<NavigationItem link="/auth">Authenticate</NavigationItem>
 		</ul>
 	);
 }
