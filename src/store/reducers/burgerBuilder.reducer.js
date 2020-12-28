e8cd06af92466f7b31aa8afa5562d3c211b76a25
frasnym <frastyawan.nym@gmail.com>
2020-12-28 13:57:50 +0800

    More Code Refactor: Reducers

--- src/store/reducers/burgerBuilder.reducer.js
+++ src/store/reducers/burgerBuilder.reducer.js
@@ -15,36 +15,40 @@ const INGREDIENT_PRICES = {
 	meat: 1.3,
 };
 
+const addIngredient = (state, action) => {
+	const updatedIngredient = {
+		[action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+	};
+	const updatedIngredients = updateObject(
+		state.ingredients,
+		updatedIngredient
+	);
+	const updatedState = {
+		ingredients: updatedIngredients,
+		totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+	};
+	return updateObject(state, updatedState);
+};
+
+const removeIngredient = (state, action) => {
+	const updatedIng = {
+		[action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+	};
+	const updatedIngs = updateObject(state.ingredients, updatedIng);
+	const updatedSt = {
+		ingredients: updatedIngs,
+		totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+	};
+	return updateObject(state, updatedSt);
+};
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.ADD_INGREDIENT:
-			const updatedIngredient = {
-				[action.ingredientName]:
-					state.ingredients[action.ingredientName] + 1,
-			};
-			const updatedIngredients = updateObject(
-				state.ingredients,
-				updatedIngredient
-			);
-			const updatedState = {
-				ingredients: updatedIngredients,
-				totalPrice:
-					state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-			};
-			return updateObject(state, updatedState);
+			return addIngredient(state, action);
 
 		case actionTypes.REMOVE_INGREDIENT:
-			const updatedIng = {
-				[action.ingredientName]:
-					state.ingredients[action.ingredientName] - 1,
-			};
-			const updatedIngs = updateObject(state.ingredients, updatedIng);
-			const updatedSt = {
-				ingredients: updatedIngs,
-				totalPrice:
-					state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-			};
-			return updateObject(state, updatedSt);
+			return removeIngredient(state, action);
 
 		case actionTypes.FETCH_INGREDIENTS_SUCCESS:
 			return updateObject(state, {
