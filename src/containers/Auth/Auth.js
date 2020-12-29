3adac026705b4e73af3496032c1437d8f20236b0
frasnym <frastyawan.nym@gmail.com>
2020-12-29 11:48:42 +0800

    Code Refactor: checkValidity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -8,7 +8,7 @@ import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import { auth, setAuthRedirectPath } from "../../store/actions/actions";
-import { updateObject } from "../../utils/utility";
+import { updateObject, checkValidity } from "../../utils/utility";
 
 class Auth extends Component {
 	state = {
@@ -51,33 +51,11 @@ class Auth extends Component {
 		}
 	}
 
-	checkValidity(value, rules) {
-		let isValid = true;
-
-		if (!rules) {
-			return isValid;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== isValid;
-		}
-
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangedHandler = (event, controlName) => {
 		const updatedControls = updateObject(this.state.controls, {
 			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
-				valid: this.checkValidity(
+				valid: checkValidity(
 					event.target.value,
 					this.state.controls[controlName].validation
 				),
