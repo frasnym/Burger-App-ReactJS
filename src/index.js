a2e512ff8bea8761b76350e2de18124ffce75c45
frasnym <frastyawan.nym@gmail.com>
2020-12-29 11:52:39 +0800

    Using .env

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import rootReducer from "./store/reducers/reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+	process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 const store = createStore(
 	rootReducer,
 	composeEnhancers(applyMiddleware(thunk))
