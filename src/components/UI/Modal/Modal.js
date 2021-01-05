8b67246e2deed210b4f63a6575fae5b684402ddc
frasnym <frastyawan.nym@gmail.com>
2021-01-05 14:48:22 +0800

    More Convert

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,36 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Backdrop from "../Backdrop/Backdrop";
 import "./Modal.css";
 
-export default class Modal extends Component {
-	shouldComponentUpdate(nextProps, nextState) {
-		// Don't update if modal not shown
-		return (
-			nextProps.show !== this.props.show ||
-			nextProps.children !== this.props.children
-		);
-	}
+const Modal = (props) => {
+	return (
+		<>
+			<Backdrop show={props.show} clicked={props.modalClose} />
+			<div
+				className="Modal"
+				style={{
+					transform: props.show
+						? "translateY(0)"
+						: "translateY(-100vh)",
+					opacity: props.show ? "1" : "0",
+				}}
+			>
+				{props.children}
+			</div>
+		</>
+	);
+};
 
-	render() {
-		return (
-			<>
-				<Backdrop
-					show={this.props.show}
-					clicked={this.props.modalClose}
-				/>
-				<div
-					className="Modal"
-					style={{
-						transform: this.props.show
-							? "translateY(0)"
-							: "translateY(-100vh)",
-						opacity: this.props.show ? "1" : "0",
-					}}
-				>
-					{this.props.children}
-				</div>
-			</>
-		);
-	}
-}
+export default React.memo(
+	Modal,
+	(prevProps, nextProps) =>
+		nextProps.show === prevProps.show &&
+		nextProps.children === prevProps.children
+);
